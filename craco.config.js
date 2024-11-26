7ba14686d3fb5df8f93dbf85ae7624e11fd0c4c5
Ahmed Jalal <ahmed.jalal@itb.local>
2024-11-26 11:50:26 +0500

    first commit

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,38 @@
-module.exports = {
-    webpack: {
-      // Add custom Webpack configurations here if needed
-    },
-    plugins: [
-      // Example: Add CRACO plugins here
-    ],
-  };
+// module.exports = {
+//     webpack: {
+//       // Add custom Webpack configurations here if needed
+//     },
+//     plugins: [
+//       // Example: Add CRACO plugins here
+//     ],
+//   };
   
+
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      if (process.env.NODE_ENV === "production") {
+        webpackConfig.optimization = {
+          ...webpackConfig.optimization,
+          minimize: true,
+          minimizer: [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true, // Remove console logs
+                },
+                output: {
+                  comments: false, // Remove comments
+                },
+              },
+            }),
+          ],
+        };
+      }
+      return webpackConfig;
+    },
+  },
+};
